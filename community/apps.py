1d0c07a8d78851b50054369734d51d6bcef60f77
Pratap Bhanu Solanki <prabhanu@Prataps-MacBook-Pro.local>
2017-01-21 16:02:04 -0500

    added all models basic schema

--- community/apps.py
+++ community/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class ClubsConfig(AppConfig):
-    name = 'clubs'
+class CommunityConfig(AppConfig):
+    name = 'community'
---
+++ community/models.py
@@ -0,0 +1,67 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    major = models.ForeignKey(Major)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    type = models.ForeignKey(Type)
+
+    def __str__(self):
+        return self.name
+
+class Major(models.Model):
+    acronym = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "%s (%s)" % (self.acronym, self.name)
+
+class Type(models.Model):
+	name = models.CharField(max_length=200)
+
+	def __str__(self):
+        return self.name
+
+class Category(models.Model):
+	name = models.CharField(max_length=200)
+
+	def __str__(self):
+        return self.name
+
+class Event(models.Model):
+	name = models.CharField(max_length=200)
+	creator = models.ForeignKey(User)
+	club = models.ForeignKey(Club)
+	categories = models.ManyToManyField(Category)
+    majors = models.ManyToManyField(Majors)
+    types = models.ManyToManyField(Type)
+	description = models.TextField()
+	location = models.CharField(max_length=400)
+	start_time = models.DateTimeField()
+	end_time = models.DateTimeField()
+	created_date = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+        return self.name
+
+
+class Club(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    categories = models.ManyToManyField(Category)
+  
+    def __str__(self):
+        return self.name
+
+
+
+
