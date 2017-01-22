7a17025d3a21a1e354f3b01ed46776420beafa39
Pratap Bhanu Solanki <prabhanu@Prataps-MacBook-Pro.local>
2017-01-22 01:49:49 -0500

    templates and all that

--- roommate_finder/admin.py
+++ roommate_finder/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Question
+from .models import Option
 
-# Register your models here.
+admin.site.register(Question)
+admin.site.register(Option)
